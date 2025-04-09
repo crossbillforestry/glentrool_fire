4374a5ebdf54628ec84fe50ed4b2bb3ad5ad38df
Ben Crisford <ben@crossbillforestry.co.uk>
2025-04-09 09:57:23 +0100

    Initial commit

---
+++ layers/layers.js
@@ -0,0 +1,56 @@
+var wms_layers = [];
+
+
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var lyr_FireseveritydNBR_1 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Fire severity (dNBR)",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/FireseveritydNBR_1.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-505873.891636, 7373596.670582, -487451.124956, 7401220.736966]
+                            })
+                        });
+var format_MeandNBRbysubcpt_2 = new ol.format.GeoJSON();
+var features_MeandNBRbysubcpt_2 = format_MeandNBRbysubcpt_2.readFeatures(json_MeandNBRbysubcpt_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_MeandNBRbysubcpt_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_MeandNBRbysubcpt_2.addFeatures(features_MeandNBRbysubcpt_2);
+var lyr_MeandNBRbysubcpt_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_MeandNBRbysubcpt_2, 
+                style: style_MeandNBRbysubcpt_2,
+                popuplayertitle: "Mean dNBR (by subcpt)",
+                interactive: true,
+    title: 'Mean dNBR (by subcpt)<br />\
+    <img src="styles/legend/MeandNBRbysubcpt_2_0.png" /> -0.05 - 0.1<br />\
+    <img src="styles/legend/MeandNBRbysubcpt_2_1.png" /> 0.1 - 0.25<br />\
+    <img src="styles/legend/MeandNBRbysubcpt_2_2.png" /> 0.25 - 0.41<br />\
+    <img src="styles/legend/MeandNBRbysubcpt_2_3.png" /> 0.41 - 0.56<br />\
+    <img src="styles/legend/MeandNBRbysubcpt_2_4.png" /> 0.56 - 0.71<br />'
+        });
+
+lyr_OSMStandard_0.setVisible(true);lyr_FireseveritydNBR_1.setVisible(true);lyr_MeandNBRbysubcpt_2.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_FireseveritydNBR_1,lyr_MeandNBRbysubcpt_2];
+lyr_MeandNBRbysubcpt_2.set('fieldAliases', {'fid': 'fid', 'OBJECTID_1': 'OBJECTID_1', 'FOREST': 'Forest ID', 'COMPTMENT': 'Compartment', 'SUBCOMPT': 'Subcpt', 'BLOCK': 'BLOCK', 'PRI_SPCODE': 'Species 1', 'PRISPECIES': 'PRISPECIES', 'SEC_SPCODE': 'Species 2', 'SECSPECIES': 'SECSPECIES', 'TER_SPCODE': 'Species 3', 'TERSPECIES': 'TERSPECIES', 'PRIPCTAREA': 'PRIPCTAREA', 'SECPCTAREA': 'SECPCTAREA', 'TERPCTAREA': 'TERPCTAREA', 'CULT_CODE': 'CULT_CODE', 'CULTIVATN': 'CULTIVATN', 'PRI_LUCODE': 'PRI_LUCODE', 'PRILANDUSE': 'PRILANDUSE', 'SEC_LUCODE': 'SEC_LUCODE', 'SECLANDUSE': 'SECLANDUSE', 'TER_LUCODE': 'TER_LUCODE', 'TERLANDUSE': 'TERLANDUSE', 'PRIHABCODE': 'PRIHABCODE', 'PRIHABITAT': 'PRIHABITAT', 'SECHABCODE': 'SECHABCODE', 'SECHABITAT': 'SECHABITAT', 'TERHABCODE': 'TERHABCODE', 'TERHABITAT': 'TERHABITAT', 'PRI_PLYEAR': 'Planting year', 'SEC_PLYEAR': 'SEC_PLYEAR', 'TER_PLYEAR': 'TER_PLYEAR', 'PRI_YIELD': 'PRI_YIELD', 'SEC_YIELD': 'SEC_YIELD', 'TER_YIELD': 'TER_YIELD', 'Shape__Are': 'Shape__Are', 'Shape__Len': 'Shape__Len', 'dnbr_mean': 'dNBR Mean', 'dnbr_median': 'dNBR Median', 'dnbr_stdev': 'dNBR Stdev', 'dnbr_min': 'dNBR Min', 'dnbr_max': 'dNBR Max', 'image': 'image', });
+lyr_MeandNBRbysubcpt_2.set('fieldImages', {'fid': 'TextEdit', 'OBJECTID_1': 'Range', 'FOREST': 'Range', 'COMPTMENT': 'Range', 'SUBCOMPT': 'TextEdit', 'BLOCK': 'Range', 'PRI_SPCODE': 'TextEdit', 'PRISPECIES': 'TextEdit', 'SEC_SPCODE': 'TextEdit', 'SECSPECIES': 'TextEdit', 'TER_SPCODE': 'TextEdit', 'TERSPECIES': 'TextEdit', 'PRIPCTAREA': 'Range', 'SECPCTAREA': 'Range', 'TERPCTAREA': 'Range', 'CULT_CODE': 'TextEdit', 'CULTIVATN': 'TextEdit', 'PRI_LUCODE': 'TextEdit', 'PRILANDUSE': 'TextEdit', 'SEC_LUCODE': 'TextEdit', 'SECLANDUSE': 'TextEdit', 'TER_LUCODE': 'TextEdit', 'TERLANDUSE': 'TextEdit', 'PRIHABCODE': 'TextEdit', 'PRIHABITAT': 'TextEdit', 'SECHABCODE': 'TextEdit', 'SECHABITAT': 'TextEdit', 'TERHABCODE': 'TextEdit', 'TERHABITAT': 'TextEdit', 'PRI_PLYEAR': 'Range', 'SEC_PLYEAR': 'Range', 'TER_PLYEAR': 'Range', 'PRI_YIELD': 'Range', 'SEC_YIELD': 'Range', 'TER_YIELD': 'Range', 'Shape__Are': 'TextEdit', 'Shape__Len': 'TextEdit', 'dnbr_mean': 'TextEdit', 'dnbr_median': 'TextEdit', 'dnbr_stdev': 'TextEdit', 'dnbr_min': 'TextEdit', 'dnbr_max': 'TextEdit', 'image': 'ExternalResource', });
+lyr_MeandNBRbysubcpt_2.set('fieldLabels', {'fid': 'hidden field', 'OBJECTID_1': 'hidden field', 'FOREST': 'inline label - visible with data', 'COMPTMENT': 'inline label - visible with data', 'SUBCOMPT': 'inline label - visible with data', 'BLOCK': 'hidden field', 'PRI_SPCODE': 'inline label - visible with data', 'PRISPECIES': 'hidden field', 'SEC_SPCODE': 'inline label - visible with data', 'SECSPECIES': 'hidden field', 'TER_SPCODE': 'inline label - visible with data', 'TERSPECIES': 'hidden field', 'PRIPCTAREA': 'hidden field', 'SECPCTAREA': 'hidden field', 'TERPCTAREA': 'hidden field', 'CULT_CODE': 'hidden field', 'CULTIVATN': 'hidden field', 'PRI_LUCODE': 'hidden field', 'PRILANDUSE': 'hidden field', 'SEC_LUCODE': 'hidden field', 'SECLANDUSE': 'hidden field', 'TER_LUCODE': 'hidden field', 'TERLANDUSE': 'hidden field', 'PRIHABCODE': 'hidden field', 'PRIHABITAT': 'hidden field', 'SECHABCODE': 'hidden field', 'SECHABITAT': 'hidden field', 'TERHABCODE': 'hidden field', 'TERHABITAT': 'hidden field', 'PRI_PLYEAR': 'inline label - visible with data', 'SEC_PLYEAR': 'hidden field', 'TER_PLYEAR': 'hidden field', 'PRI_YIELD': 'hidden field', 'SEC_YIELD': 'hidden field', 'TER_YIELD': 'hidden field', 'Shape__Are': 'hidden field', 'Shape__Len': 'hidden field', 'dnbr_mean': 'inline label - visible with data', 'dnbr_median': 'inline label - visible with data', 'dnbr_stdev': 'inline label - visible with data', 'dnbr_min': 'inline label - visible with data', 'dnbr_max': 'inline label - visible with data', 'image': 'no label', });
+lyr_MeandNBRbysubcpt_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/MeandNBRbysubcpt_2_style.js
@@ -0,0 +1,61 @@
+var size = 0;
+var placement = 'point';
+
+var style_MeandNBRbysubcpt_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("dnbr_mean");
+    var labelText = "";
+    size = 0;
+    var labelFont = "13.0px \'Liberation Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= -0.049998 && value <= 0.102019) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,245,240,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.102019 && value <= 0.254037) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(252,190,165,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.254037 && value <= 0.406054) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(251,112,80,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.406054 && value <= 0.558071) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(211,32,32,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.558071 && value <= 0.710089) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(103,0,13,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
