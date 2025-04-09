4bcb4ed71c57951b9aed28ab6d099f48ece0037d
Ben Crisford <ben@crossbillforestry.co.uk>
2025-04-09 10:22:45 +0100

    Update attribution text

--- layers/layers.js
+++ layers/layers.js
@@ -9,6 +9,7 @@ var wms_layers = [];
             
             source: new ol.source.XYZ({
     attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a><br />\
+    <a href="https://https://www.copernicus.eu/en">Contains modified Copernicus Sentinel data 2025</a><br/>\
     <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Contains public sector information licensed under the Open Government Licence v3.0</a><br />\
     <a href="https://spatialdata.gov.scot/geonetwork/srv/eng/catalog.search#/metadata/4ea30596-8d2c-4be7-a5c3-ecd4fc580a3b">Reproduced by Permission of Ordnance Survey on behalf of HMSO.<br/>© Crown copyright and database right 2025. Ordnance Survey Licence number 100021242</a>',
                 url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
